bf22033db24754376793d47285a6f5fa9828c8a1
arunptl100 <arunptl100@hotmail.com>
2020-06-16 03:17:12 +0100

    added more comments and details of 2-CNF implementation

--- sat-solver.py
+++ sat-solver.py
@@ -13,12 +13,14 @@ class dir_graph:
 
     # Function that adds an edge (u,v) to the graph
     #  It finds the dictionary entry for node u and appends node v to its list
+    # performance: O(1)
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
     # Function that outputs the edges of all nodes in the graph
     #  prints all (u,v) in the set of edges of the graoh
-    def generate_edges(self):
+    # performance: O(m+n) m = #edges , n = #nodes
+    def print(self):
         edges = []
         # for each node in graph
         for node in self.graph:
@@ -28,9 +30,22 @@ class dir_graph:
                 edges.append((node, neighbour))
         return edges
 
+# 2-CNF class
+#  Class storing a boolean formula in Conjunctive Normal Form of literals
+#  where the size of clauses is at most 2
+#  -NOTATION-
+#    The CNF is represented as a list of lists
+#    e.g [[x, y], [k, z]] == (x or y) and (k or z)
+#    i.e Conjunction of inner lists , where the inner lists are disjunctions
+#    of literals
+#
+# class two_cnf:
+
+
+
 
 g = dir_graph()
 g.addEdge('u', 'v')
 g.addEdge('v', 'x')
 g.addEdge('x', 'z')
-print(g.generate_edges())
+print(g.print())
