82d69fad9396da5de6f42d23c47be58ab6dc1976
arunptl100 <arunptl100@hotmail.com>
2020-06-17 02:56:56 +0100

    added class for 2-CNF and function that generates a graph based on the 2-CNF

--- sat-solver.py
+++ sat-solver.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-
+neg = '~'
 # directed graph class
 #  adapted from:
 #  src: https://www.geeksforgeeks.org/generate-graph-using-dictionary-python/
@@ -30,6 +30,7 @@ class dir_graph:
                 edges.append((node, neighbour))
         return edges
 
+
 # 2-CNF class
 #  Class storing a boolean formula in Conjunctive Normal Form of literals
 #  where the size of clauses is at most 2
@@ -38,14 +39,58 @@ class dir_graph:
 #    e.g [[x, y], [k, z]] == (x or y) and (k or z)
 #    i.e Conjunction of inner lists , where the inner lists are disjunctions
 #    of literals
-#
+#    Negation is represented with ~ .  ~x == negation of literal x
 # class two_cnf:
+class two_cnf:
+    def __init__(self):
+        self.con = []
+
+    # adds a clause to the CNF
+    # performance O(1)
+    def add_clause(self, clause):
+        if len(clause) <= 2:
+            self.con.append(clause)
+        else:
+            print("error: clause contains > 2 literals")
+
+    # returns a set of all the variables in the CNF formula
+    def get_variables(self):
+        vars = set()
+        for clause in self.con:
+            for literal in clause:
+                vars.add(literal)
+        return vars
+
+    def print(self):
+        print(self.con)
 
 
+def double_neg(formula):
+    if (neg+neg) in formula:
+        return formula.replace((neg+neg), '')
+    else:
+        return formula
 
 
-g = dir_graph()
-g.addEdge('u', 'v')
-g.addEdge('v', 'x')
-g.addEdge('x', 'z')
-print(g.print())
+def two_sat_solver(two_cnf_formula):
+    # setup the edges of the graph
+    # G = (V,E) , V = L U ~L where L = set of variables in 2-CNF
+    # E =
+    # {(~u,v),(~v,u) | for all clauses [u,v] } U {(~u,u) | for all clauses [u]}
+    graph = dir_graph()
+    for clause in two_cnf_formula.con:
+        if len(clause) == 2:
+            u = clause[0]
+            v = clause[1]
+            graph.addEdge(double_neg(neg+u), v)
+            graph.addEdge(double_neg(neg+v), u)
+        else:
+            graph.addEdge(double_neg(neg+clause[0]), clause[0])
+    print(graph.print())
+
+
+# ======= 2-CNF setup =======
+formula = two_cnf()
+formula.add_clause(['x', 'y'])
+formula.add_clause(['y', '~z'])
+two_sat_solver(formula)
