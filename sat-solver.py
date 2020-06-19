340c700d79a9b77581024e1f2e653720468854d9
arunptl100 <arunptl100@hotmail.com>
2020-06-19 23:39:25 +0100

    imlplemented DFS and a fn that computes the transpose of a graph

--- sat-solver.py
+++ sat-solver.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
 neg = '~'
+
+
 # directed graph class
 #  adapted from:
 #  src: https://www.geeksforgeeks.org/generate-graph-using-dictionary-python/
@@ -9,13 +11,16 @@ class dir_graph:
         # create an empty directed graph, represented by a dictionary
         #  The dictionary consists of keys and corresponding lists
         #  Key = node u , List = nodes, v, such that (u,v) is an edge
-        self.graph = defaultdict(list)
+        self.graph = defaultdict(set)
+        self.nodes = set()
 
     # Function that adds an edge (u,v) to the graph
     #  It finds the dictionary entry for node u and appends node v to its list
     # performance: O(1)
     def addEdge(self, u, v):
-        self.graph[u].append(v)
+        self.graph[u].add(v)
+        self.nodes.add(u)
+        self.nodes.add(v)
 
     # Function that outputs the edges of all nodes in the graph
     #  prints all (u,v) in the set of edges of the graoh
@@ -71,6 +76,38 @@ def double_neg(formula):
     return formula.replace((neg+neg), '')
 
 
+# Function that performs Depth First Search on a directed graph
+# O(|V|+|E|)
+def DFS(dir_graph, visited, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in dir_graph.graph[node]:
+            DFS(dir_graph, visited, neighbour)
+    return visited
+
+
+# Function that generates the transpose of a given directed graph
+# Performance O(|V|+|E|)
+def transpose_graph(d_graph):
+    t_graph = dir_graph()
+    # for each node in graph
+    for node in d_graph.graph:
+        # for each neighbour node of a single node
+        for neighbour in d_graph.graph[node]:
+            t_graph.addEdge(neighbour, node)
+    return t_graph
+
+
+# Function that finds all the strongly connected components in a given graph
+# Implementation of Kosaraju’s algorithm
+# Performance O(|V|+|E|) for a directed graph G=(V,E)
+def strongly_connected_components(dir_graph):
+    stack = []
+    pass
+
+
+# Function that determines if a given 2-CNF is Satisfiable or not
 def two_sat_solver(two_cnf_formula):
     # setup the edges of the graph
     # G = (V,E) , V = L U ~L where L = set of variables in 2-CNF
@@ -89,7 +126,10 @@ def two_sat_solver(two_cnf_formula):
 
 
 # ======= 2-CNF setup =======
-formula = two_cnf()
-formula.add_clause(['x', 'y'])
-formula.add_clause(['y', '~z'])
-two_sat_solver(formula)
+g = dir_graph()
+g.addEdge('u', 'v')
+g.addEdge('v', 'x')
+g.addEdge('x', 'z')
+print(g.print())
+print(transpose_graph(g).print())
+# DFS(g, set(), 'u')
